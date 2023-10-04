6f7854a6864a6b626f801bb0ae2551a30a208f5e
RajeevNShetty <rajeevshetty@sharechat.co>
2023-10-04 19:33:47 +0530

    Added Shuffle string logic.

--- app/src/main/java/com/rajeev/machinecoding/machinecoding/common/StringExtension.kt
+++ app/src/main/java/com/rajeev/machinecoding/machinecoding/common/StringExtension.kt
@@ -2,6 +2,6 @@ package com.rajeev.machinecoding.machinecoding.common
 
 fun String.getRandomCharsIncludingString() : List<Char> {
     val givenString = this.toList()
-    val randomString = "qwetrty".toList() // TODO method to generate a random string
-    return (givenString + randomString)
+    val randomString = givenString + "qwetrty".toList() // TODO method to generate a random string
+    return randomString.shuffled()
 }
